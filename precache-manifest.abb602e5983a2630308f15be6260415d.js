aa70e67233b1aa87fd9f8c6431e46030ae241e7d
Johan Tinglöf <johan.tinglof@rebelandbird.com>
2020-09-27 18:02:31 +0200

    Intial commit for hrv breathing bundle

---
+++ precache-manifest.abb602e5983a2630308f15be6260415d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8ef0af06600155d8a5e85bb9ad92120",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98a37735befa40d7395a",
+    "url": "/static/css/2.f7557379.chunk.css"
+  },
+  {
+    "revision": "53337988adf39d7ab50c",
+    "url": "/static/css/main.ce7edf64.chunk.css"
+  },
+  {
+    "revision": "98a37735befa40d7395a",
+    "url": "/static/js/2.9d621fa5.chunk.js"
+  },
+  {
+    "revision": "5ed224df42d424350687d14de0a89d1d",
+    "url": "/static/js/2.9d621fa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53337988adf39d7ab50c",
+    "url": "/static/js/main.8911b03d.chunk.js"
+  },
+  {
+    "revision": "c920d02fc2c965e36e06",
+    "url": "/static/js/runtime-main.8b0205d8.js"
+  },
+  {
+    "revision": "680b8569a81f326261489e09deda6a9d",
+    "url": "/static/media/PermanentMarker-Regular.680b8569.woff2"
+  },
+  {
+    "revision": "cad8b4747e00997f345612328e66a72e",
+    "url": "/static/media/PermanentMarker-Regular.cad8b474.woff"
+  },
+  {
+    "revision": "3386ebaeca1d264a57590690774c132b",
+    "url": "/static/media/bell_one.3386ebae.webm"
+  },
+  {
+    "revision": "451898a0f7586e945889c280a3016cb3",
+    "url": "/static/media/bell_two.451898a0.webm"
+  },
+  {
+    "revision": "5cc3aae674ea3b199313b3b83bd795bc",
+    "url": "/static/media/source-sans-pro-latin-400-normal.5cc3aae6.woff"
+  },
+  {
+    "revision": "899c8f78ce650d4009d42443897aa723",
+    "url": "/static/media/source-sans-pro-latin-400-normal.899c8f78.woff2"
+  },
+  {
+    "revision": "1a4bcb3ec9c508d478d4dbf6b56f6208",
+    "url": "/static/media/source-sans-pro-latin-700-normal.1a4bcb3e.woff2"
+  },
+  {
+    "revision": "b03f2ec28f8e60e61974dd8c57610e5b",
+    "url": "/static/media/source-sans-pro-latin-700-normal.b03f2ec2.woff"
+  },
+  {
+    "revision": "a6f749c85b751ef81fd99c6423c5ff4f",
+    "url": "/static/media/source-sans-pro-latin-900-normal.a6f749c8.woff"
+  },
+  {
+    "revision": "d8eab344801820b04142708a330623fc",
+    "url": "/static/media/source-sans-pro-latin-900-normal.d8eab344.woff2"
+  }
+]);
